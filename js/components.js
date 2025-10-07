618ad5e3badd8c1bad6155dcee8fd7d1f07cb07d
Roman Kriuchkov <157597992+RoMmMaNnN@users.noreply.github.com>
2025-10-07 16:13:44 +0100

    Fixing path

--- js/components.js
+++ js/components.js
@@ -1,13 +1,18 @@
-// ======= LOAD COMPONENTS =======
 async function loadComponent(id, file) {
   const element = document.getElementById(id);
   if (!element) return;
+
   const response = await fetch(file);
   const html = await response.text();
   element.innerHTML = html;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadComponent("header", "/pages/partials/header.html");
-  loadComponent("footer", "/pages/partials/footer.html");
+  if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/CV-Portfolio/") {
+    loadComponent("header", "pages/partials/header.html");
+    loadComponent("footer", "pages/partials/footer.html");
+  } else {
+    loadComponent("header", "../pages/partials/header.html");
+    loadComponent("footer", "../pages/partials/footer.html");
+  }
 });
